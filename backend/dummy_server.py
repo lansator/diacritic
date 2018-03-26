cfebd126eacb94dd95267939cccb8fc8383ccd4e
xalex <alexandru.oprisan@gmail.com>
2018-03-26 23:07:40 +0300

    read spelling data from redis

--- backend/dummy_server.py
+++ backend/dummy_server.py
@@ -5,7 +5,12 @@ from flask_cors import CORS
 from os.path import join
 import pickle
 from keras.preprocessing.text import text_to_word_sequence
+import json
+import operator
 
+import redis
+
+r=redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask("diacritic")
 CORS(app)
@@ -21,8 +26,17 @@ with open(join(DATA_PATH,SPELLINGS_FILE), 'rb') as pickle_file:
 print("starting")
 print(spellings_lower['acasa'])
 
+
 @app.route('/', methods=['POST'])
 def dummy_response():
+
+
+    def choose_best_alternative( alt_d ):
+        
+        s = max(alt_d.keys(), key=lambda x: alt_d[x])
+        return s
+
+
 
     request_data = request.get_json()
     type(request_data)
@@ -43,13 +57,22 @@ def dummy_response():
                                         filters='!"#$%&()*+,./:;<=>?@[\\]^_`{|}~\t\n',
                                         lower=False):
         # print(f"{input_word}: {spellings_lower[input_word.lower()]}")
-        found_stats = spellings_lower[input_word.lower()]
-        response_stats.append(found_stats)
-        w,c  = found_stats.most_common(1)[0]
-        print("sugestinon ", w)
-        response_text.append(w)
+        # found_stats = spellings_lower[input_word.lower()]
+        found_record = r.get(input_word.lower())
+        if found_record:
+            d = json.loads(found_record)
+            #print(d)
+            #print(type(d))
+            response_stats.append(d)
+            suggested = choose_best_alternative(d)
+            #w,c  = found_stats.most_common(1)[0]
+            print("sugestinon ", suggested)
+            response_text.append(suggested)
+        else:
+            print("missing word ", input_word)
+            response_text.append(input_word)
 
-    print(response_stats)
+    #print(response_stats)
     print(" ".join(response_text))
 
 
